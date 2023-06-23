112e1488b8f9299fad3e6e0aa5b6b856f60778b5
Mr-umesh <umeshkapse15@gmail.com>
2023-06-23 12:50:13 +0530

    upadte in file

--- gallery.php
+++ gallery.php
@@ -4,11 +4,13 @@
   <meta Content-Type:html,text; charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PIXL VIsual Effects - Gallery</title>
   <!-- Font links -->
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
   <!-- Font Awesome -->
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   
+  <link rel="icon" href="images/favicon.png" sizes="32x32" />
    <!-- Icon Font Stylesheet -->
    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
   <!-- CSS links-->
--- index.php
+++ index.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>pixl</title>
+    <title>PIXL Visual Effects - Home</title>
 
+    <link rel="icon" href="images/favicon.png" sizes="32x32" />
     <!-- css file link -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/lightbox.css">
